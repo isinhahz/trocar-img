3880c5f65fa8cbc2e5a62d6cd2460fde097a85b1
isinhahz <25132976@dsn127m1242315.cfp127aluno.local>
2025-08-14 13:05:01 -0300

    projeto em andamento

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 'use strict'
 
-const botaoTrocarBandeira = document.getElementById('trocar-img')
+const botaoTrocarBandeira = document.getElementById('trocar-img');
+const inputPais = document.getElementById('img');
+const body = document.body;
+
+const bandeiras = {
+    "argentina": "./img/bandeira-argentina.avif",
+    "brasil": "./img/bandeira-brasil.jpg",
+    "bolívia": "./img/bandeira-bolivia.jpg",
+    "chile": "./img/bandeira-chile.jpg",
+    "colombia": "./img/bandeira-colombia.jpg",
+    "equador": "./img/bandeira-equador.avif",
+    "guiana": "./img/bandeira-guiana.jpg",
+    "paraguai": "./img/bandeira-paraguai.jpg",
+    "peru": "./img/bandeira-peru.jpg",
+    "suriname": "./img/bandeira-suriname.jpg",
+    "uruguai": "./img/bandeira-uruguai.jpg",
+    "venezuela": "./img/bandeira-venezuela.jpg",
+}
 
 function trocarImg (){
-    const img = document.getElementById('img').image
-    document.documentElement.style.setProperty('--bg-img', Image)
+    const nomePais = inputPais.value.toLowerCase();
+    const urlBandeira = bandeiras[nomePais];
+
+    if (urlBandeira) {
+        body.style.backgroundImage = `url(${urlBandeira})`;
+    }
 }
-botaoTrocarBandeira.addEventListener('click', trocarImg)
+botaoTrocarBandeira.addEventListener('click', trocarImg);
