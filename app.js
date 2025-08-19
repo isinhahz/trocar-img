aed1c5f323af1abbff14c74301b35591a5cad3db
ISABELLE DOS SANTOS DE ABREU <25132976@CFP127ALUNO.LOCAL>
2025-08-19 14:40:56 -0300

    implementando funções no botão automático

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const botaoTrocarBandeira = document.getElementById('trocar-img');
 const inputPais = document.getElementById('img');
 const body = document.body;
+const botaoAutomatico = document.getElementById('automatico');
 
 const bandeiras = {
     "argentina": "./img/bandeira-argentina.avif",
@@ -27,4 +28,41 @@ function trocarImg (){
         body.style.backgroundImage = `url(${urlBandeira})`;
     }
 }
+
+// declarando variavel de intervalo automatico
+let pararInterval;
+let emModoAutomatico = false;
+let indicePais = 0;
+
+function alternarAutomatico(){
+    if (emModoAutomatico) {
+        // parar intervalo quando estiver em modo automatico
+        clearInterval(pararInterval);
+        botaoAutomatico.textContent = 'Automático';
+        emModoAutomatico = false;
+    } else{
+        // modo automatico executando, criação do botao parar
+        botaoAutomatico.textContent = 'Parar';
+        emModoAutomatico = true;
+        // pega os nomes dos paises e transforma em uma lista
+        const paises = Object.keys(bandeiras);
+
+        pararInterval = setInterval(() => {
+            const paisAtual = paises[indicePais];
+            const urlBandeira = bandeiras[paisAtual];
+
+        body.style.backgroundImage = `url(${urlBandeira})`;
+        //aparece os nomes dos paises ao trocar a bandeira
+        inputPais.value = paisAtual.charAt(0).toUpperCase() + paisAtual.slice(1);
+        indicePais++;
+
+        //retorna todos os paises
+        if (indicePais >= paises.length) {
+            indicePais = 0;
+         }
+      }, 3000); //3 segundos
+   }
+}
+
 botaoTrocarBandeira.addEventListener('click', trocarImg);
+botaoAutomatico.addEventListener('click', alternarAutomatico);
